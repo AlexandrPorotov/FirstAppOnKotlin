3b5fbfe1169317278f16b9555db5e78f3a54b995
Alexandr P <aleksandr.porotov@bimeister.com>
2024-03-12 23:06:08 +0300

    Initial commit

---
+++ app/src/main/java/com/example/firstapponkotlin/SampleData.kt
@@ -0,0 +1,72 @@
+import com.example.firstapponkotlin.Message
+
+object SampleData {
+    // Sample conversation data
+    val conversationSample = listOf(
+        Message(
+            "Sasha",
+            "Test...Test...Test..."
+        ),
+        Message(
+            "Sasha",
+            """List of Android versions:
+            |Android KitKat (API 19)
+            |Android Lollipop (API 21)
+            |Android Marshmallow (API 23)
+            |Android Nougat (API 24)
+            |Android Oreo (API 26)
+            |Android Pie (API 28)
+            |Android 10 (API 29)
+            |Android 11 (API 30)
+            |Android 12 (API 31)""".trim()
+        ),
+        Message(
+            "Sasha",
+            """I think Kotlin is my favorite programming language.
+            |It's so much fun!""".trim()
+        ),
+        Message(
+            "Sasha",
+            "Searching for alternatives to XML layouts..."
+        ),
+        Message(
+            "Sasha",
+            """Hey, take a look at Jetpack Compose, it's great!
+            |It's the Android's modern toolkit for building native UI.
+            |It simplifies and accelerates UI development on Android.
+            |Less code, powerful tools, and intuitive Kotlin APIs :)""".trim()
+        ),
+        Message(
+            "Sasha",
+            "It's available from API 21+ :)"
+        ),
+        Message(
+            "Sasha",
+            "Writing Kotlin for UI seems so natural, Compose where have you been all my life?"
+        ),
+        Message(
+            "Sasha",
+            "Android Studio next version's name is Arctic Fox"
+        ),
+        Message(
+            "Sasha",
+            "Android Studio Arctic Fox tooling for Compose is top notch ^_^"
+        ),
+        Message(
+            "Sasha",
+            "I didn't know you can now run the emulator directly from Android Studio"
+        ),
+        Message(
+            "Sasha",
+            "Compose Previews are great to check quickly how a composable layout looks like"
+        ),
+        Message(
+            "Sasha",
+            "Previews are also interactive after enabling the experimental setting"
+        ),
+        Message(
+            "Sasha",
+            "Have you tried writing build.gradle with KTS?"
+        ),
+    )
+}
